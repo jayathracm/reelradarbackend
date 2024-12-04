bceccb174c62630e9459dfb7b912f6499ad03791
jayathracm <jayathracm@gmail.com>
2024-12-04 22:18:52 +0200

    added test print

--- index.js
+++ index.js
@@ -16,6 +16,11 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+app.get('/test', (req, res) => {
+  res.send('Hello from backend');
+});
+
 app.use('/user', userRouter);
 app.use('/reviews', reviewRouter);
 app.use("/api/reviews", reviewRouter);
