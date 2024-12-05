54b4ca66a3a3dcf2de5f0b11540ee7207b63973f
jayathracm <jayathracm@gmail.com>
2024-12-06 01:28:29 +0200

    added dependencies

--- server.cjs
+++ server.cjs
@@ -1,4 +1,7 @@
-import('./index.js').catch(err => {
+// server.cjs
+try {
+    await import('./index.js');
+  } catch (err) {
     console.error('Error importing ES module:', err);
     process.exit(1);
-  });
+  }
